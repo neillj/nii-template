2bca7d06a00c9c2e7a4345c87fd977138b0ec8c8
Joe Woodhouse <joe.woodhouse@newicon.net>
2013-01-16 15:56:30 +0000

    Remove deployment code, add deploy dependency to composer.json. Move all code down one level

--- deploy/config/boot.rb
+++
@@ -1,9 +0,0 @@
-require 'yaml'
-
-DEPLOY_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-REPO_ROOT = File.expand_path(File.join(DEPLOY_ROOT, '..'))
-DEPLOY_CONFIG = YAML::load(File.open(File.join(DEPLOY_ROOT, 'config', 'config.yml')))
-
-require File.join(DEPLOY_ROOT, 'lib', 'helpers', 'task_helper')
-
-include TaskHelper
--- deploy/config/deploy/production.rb
+++
@@ -1,7 +0,0 @@
-production_domain = "hub.newicon.net"
-
-set :environment, 'production'
-
-role :app, production_domain
-role :web, production_domain
-role :db, production_domain, :primary => true
